9b824847d918c0d680f01d03c7c730c981d9163d
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2021-10-20 18:23:39 -0300

    Aula: Interface de Detalhes do Agendamento

--- App.tsx
+++ App.tsx
@@ -13,7 +13,7 @@ import {
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
-import { Scheduling } from './src/screens/Scheduling';
+import { SchedulingDetails } from './src/screens/SchedulingDetails';
 import theme from './src/styles/theme';
 
 export default function App() {
@@ -31,7 +31,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Scheduling />
+      <SchedulingDetails />
     </ThemeProvider>
   );
 }
