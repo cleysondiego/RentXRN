a41a51fb0bac31a54e373f8da6c755b712bdee6b
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2021-11-01 00:15:41 -0300

    Aula: Input de Senha

--- src/components/PasswordInput/styles.ts
+++ src/components/PasswordInput/styles.ts
@@ -2,11 +2,10 @@ import { TextInput } from 'react-native';
 
 import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { BorderlessButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex-direction: row;
-
-  margin-bottom: 8px;
 `;
 
 export const IconContainer = styled.View`
