bbd44e79124320b0fe16c232749b5919eb431c30
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2021-11-04 00:34:36 -0300

    Aula: Atualizando Perfil

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -34,6 +34,7 @@ interface AuthContextData {
   user: User;
   signIn: (credentials: SignInCredentials) => Promise<void>;
   signOut: () => Promise<void>;
+  updateUser: (user: User) => Promise<void>;
 }
 
 interface AuthProviderProps {
@@ -87,6 +88,24 @@ function AuthProvider({ children }: AuthProviderProps) {
     }
   }
 
+  async function updateUser(user: User) {
+    try {
+      const userCollection = database.get<ModelUser>('users');
+      await database.write(async () => {
+        const userSelected = await userCollection.find(user.id);
+        await userSelected.update(( userData ) => {
+          userData.name = user.name;
+          userData.driver_license = user.driver_license;
+          userData.avatar = user.avatar;
+        });
+      });
+
+      setData(user);
+    } catch(error) {
+      throw new Error(error);
+    }
+  }
+
   useEffect(() => {
     async function loadUserData() {
       const userCollection = database.get<ModelUser>('users');
@@ -107,7 +126,8 @@ function AuthProvider({ children }: AuthProviderProps) {
       value={{
         user: data,
         signIn,
-        signOut
+        signOut,
+        updateUser
       }}
     >
       {children}
