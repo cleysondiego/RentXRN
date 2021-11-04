caf01de7dabe63f1f74244c722bc25f4a5252df0
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2021-11-03 23:35:30 -0300

    Aula: Header do Profile

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -7,7 +7,7 @@ import { useTheme } from 'styled-components';
 import HomeSvg from '../assets/home.svg';
 import PeopleSvg from '../assets/people.svg';
 import CarSvg from '../assets/car.svg';
-import { Home } from '../screens/Home';
+import { Profile } from '../screens/Profile';
 import { MyCars } from '../screens/MyCars';
 
 import { AppStackRoutes } from './app.stack.routes';
@@ -32,21 +32,11 @@ export function AppTabRoutes() {
       }}
     >
       <Screen
-        name="Home"
+        name="AppStackRoutes"
         component={AppStackRoutes}
         options={{
           tabBarIcon: (({ color }) => (
             <HomeSvg width={24} height={24} fill={color} />
-          ))
-        }}
-      />
-
-      <Screen
-        name="Profile"
-        component={Home}
-        options={{
-          tabBarIcon: (({ color }) => (
-            <PeopleSvg width={24} height={24} fill={color} />
           ))
         }}
       />
@@ -57,6 +47,16 @@ export function AppTabRoutes() {
         options={{
           tabBarIcon: (({ color }) => (
             <CarSvg width={24} height={24} fill={color} />
+          ))
+        }}
+      />
+
+      <Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: (({ color }) => (
+            <PeopleSvg width={24} height={24} fill={color} />
           ))
         }}
       />
